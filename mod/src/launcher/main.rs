0ef19fe260cd14451504e2c6db4cfac41d6bc5f5
Romain Bignon <romain@bignon.me>
2026-01-09 15:17:44 +0100

    mod: remove useless copyrights

--- mod/src/launcher/main.rs
+++ mod/src/launcher/main.rs
@@ -1,10 +1,4 @@
 // FogRandoTracker Launcher - GUI application for managing game sessions
-// Copyright (C) 2024 wospins
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
--- mod/src/lib.rs
+++ mod/src/lib.rs
@@ -1,22 +1,4 @@
 // FogRandoTracker - Fog Gate Randomizer Tracker for Elden Ring
-// Copyright (C) 2024 wospins
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-//
-// This project uses code from eldenring-practice-tool by johndisandonato
-// which is also licensed under AGPL-3.0.
-// Original source: https://github.com/veeenu/eldenring-practice-tool
 
 // =============================================================================
 // MODULES
